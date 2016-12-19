5eaa995ee661dcaea69531fa2beea5a1f7441d48
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-12-19 16:36:48 +0800

    Init commit with 2 demo code

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+print("Hello World!")
---
+++ if_clause.py
@@ -0,0 +1,6 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+varA = 161
+if varA % 2 == 0 :
+    print("In the positive condition")
+else:
+    print("In the negative condition")
