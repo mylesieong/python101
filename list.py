8ad6eebdb6f2bbbf9a4920d9df230e661fb59d9b
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-12-19 16:54:50 +0800

    Add list

---
+++ list.py
@@ -0,0 +1,16 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[1])
+for x in mylist:
+    print(x)
+
+# list adding
+even_list = [2,4,6]
+odd_list = [1,3,5]
+print(len(even_list))
+print(len(even_list + odd_list))
+print((even_list + odd_list)[2])
+
