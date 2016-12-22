afbf65f8cf8b7026bdf0eb4652fc965d374a23ca
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-12-22 12:47:21 +0800

    Add new lesson

--- string_format.py
+++ string_format.py
@@ -1,1 +1,10 @@
 #!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+name = "John"
+print("Hello, %s!"% name)
+
+name = "Tom"
+age = 26
+print("%s is %d years old." % (name, age))
+
+mylist = [1,2,3]
+print("A list: %s" % mylist)
---
+++ string_operation.py
@@ -0,0 +1,13 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+str1 = "Hello World!"
+print (len(str1))
+print(str1.index("o"))
+print(str1.count("l"))
+print(str1[3:8])
+print(str1[3:8:2])
+print(str1[::-1])
+print(str1.startswith("Hel"))
+print(str1.endswith("Hel"))
+print(str1.split(" "))
+print(str1.upper())
+
