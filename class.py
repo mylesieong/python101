8af1f11b4727dd108cb1c448da781bc775e0b343
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2016-12-22 12:57:11 +0800

    New codes

---
+++ class.py
@@ -0,0 +1,10 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+class MyClass:
+    var = "Foo"
+
+    def function(self):
+        print("foo bar")
+
+myObject = MyClass()
+myObject.function()
+print(myObject.var)
