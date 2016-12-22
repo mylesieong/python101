85e4fcf182c5ee330dfead0d6aecb4f6d06b4162
Myles Ieong <Myles Ieong>
2016-12-22 23:37:43 +0800

    Add function arguments and regex demo

---
+++ function_arguments.py
@@ -0,0 +1,7 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+def myfunction(first, second, third):
+	print("First is %s" % first)
+	print("Second is %s" % second)
+	print("Third is %s" % third)
+
+myfunction("Tom", "John", "Mary")
---
+++ regex_demo.py
@@ -0,0 +1,22 @@
+#!/c/Users/BI77/AppData/Local/Programs/Python/Python35-32/python
+import re
+pattern = re.compile(r"\[(on|off)\]") # Slight optimization
+print(re.search(pattern, "Mono: Playback 65 [75%] [-16.50dB]dfd [on]dfd"))
+# Returns a Match object!
+print(re.search(pattern, "Nada..[off].:-("))
+# Doesn't return anything.
+# End Example
+
+# Exercise: make a regular expression that will match an email
+def test_email(your_pattern):
+    pattern = re.compile(your_pattern)
+    emails = ["john@example.com", "python-list@python.org", "wha.t.`1an?ug{}ly@email.com"]
+    for email in emails:
+        if not re.match(pattern, email):
+            print("You failed to match %s" % (email))
+        elif not your_pattern:
+            print("Forgot to enter a pattern!")
+        else:
+            print("Pass")
+pattern = r".*@.*" # Your pattern here!
+test_email(pattern)
